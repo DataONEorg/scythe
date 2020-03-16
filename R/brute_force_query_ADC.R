6ee32bb2060bf76913ba2cd0b1800ecd2b6cc7b2
Jeanette Clark <jclark@nceas.ucsb.edu>
2020-03-16 14:00:15 -0700

    add R scripts for brute force querying

---
+++ R/brute_force_query_ADC.R
@@ -0,0 +1,55 @@
+library(jsonlite)
+library(dplyr)
+library(xml2)
+
+## bash SCOPUS queries
+# APIKEY=985f23cdfde84bdcb15229504fb644b4
+# 
+# # multi-page ADC
+# for pg in 0 26; do curl https://api.elsevier.com/content/search/scopus?query=ALL:10.18739\&date=2009-2019\&APIKey=${APIKEY}\&start=${pg} -o results/scopus-10.18739-2009-2019-pg${pg}.json; done
+# 
+# # multi-page KNB
+# for pg in 0 26; do curl https://api.elsevier.com/content/search/scopus?query==ALL:10.5063\&date=2009-2019\&APIKey=${APIKEY}\&start=${pg} -o results/scopus-10.5063-2009-2019-pg${pg}.json; done
+
+# query for ADC DOIs
+cn <- CNode("PROD")
+mn <- getMNode(cn, "urn:node:ARCTIC")
+
+result <- query(mn, list(q = "formatType:METADATA AND (*:* NOT obsoletedBy:*)",
+                                fl = "identifier,rightsHolder,formatId",
+                                start ="0",
+                                rows = "15000"),
+                                as="data.frame")
+
+dois <- grep("doi", result$identifier, value = T) %>% 
+  gsub("doi:", "", .)
+
+# brute force query SCOPUS for each DOI
+t <- list()
+for (i in 1:length(dois)){
+  t[[i]] <- fromJSON(curl(paste0("https://api.elsevier.com/content/search/scopus?query=ALL:",dois[i],"&APIKey=ae55f95a9d2f56c21147d3f9f6c4eef0")))
+}
+
+# find the number of results per DOI
+res <- lapply(t, function(x){x$`search-results`$`opensearch:totalResults`})
+
+t_working <- t[which(res != 0)]
+
+t_results <- lapply(t_working, function(x){
+  x$`search-results`$entry$search <- x$`search-results`$`opensearch:Query`$`@searchTerms`
+  return(x$`search-results`$entry)
+  })
+
+
+results <- do.call(bind_rows, t_results)
+
+results_slim <- results %>% 
+  dplyr::select(`prism:doi`, search) %>% 
+  mutate(search = gsub("ALL:", "", search)) %>% 
+  rename(journal = `prism:doi`,
+         adc_dataset = search)
+
+write.csv(results_slim, "~/dataone-citations/results/brute_force_results.csv", row.names = F)
+
+
+
---
+++ R/rsv_citation_metrics.R
@@ -0,0 +1,49 @@
+# parse SCOPUS results
+
+library(dplyr)
+library(jsonlite)
+
+files <- list.files("~/dataone-citations/results/", full.names = T) %>%
+  grep("10.18739", . ,value = T)
+
+results_doi <- list()
+for (i in 1:length(files)){
+  res <- fromJSON(files[i])
+  results_doi <- bind_rows(results_doi, res$`search-results`$entry)
+}
+
+res_cdb <- fromJSON("~/dataone-citations/results/ADC_citations_db.json")
+res_cdb <- res_cdb$resultDetails$citations
+
+files_loose <- list.files("~/dataone-citations/results/", full.names = T)
+files_loose <- files_loose[4:7]
+
+results_loose <- list()
+for (i in 1:length(files_loose)){
+  res <- fromJSON(files_loose[i])
+  results_loose <- bind_rows(results_loose, res$`search-results`$entry)
+}
+
+results_loose <- unique(results_loose) %>%
+  select(-link, -affiliation)
+
+overlap <- inner_join(results_doi, results_loose)
+
+non_overlap <- anti_join(results_doi, results_loose)
+
+
+############
+
+down <- fromJSON("results/downloads_by_country.json")
+d <- down$aggregations$pid_list$buckets$key
+d$doc_count <- down$aggregations$pid_list$buckets$doc_count
+
+codes <- read.csv("https://pkgstore.datahub.io/core/country-list/data_csv/data/d7c9d7cfb42cb69f4422dec222dbbaa8/data_csv.csv") %>% rename(country = Code)
+
+d <- left_join(d, codes)
+
+d_data <- filter(d, format == "DATA")
+d_metadata <- filter(d, format == "METADATA")
+t <- joinCountryData2Map(d_data, joinCode = "ISO2", nameJoinColumn = "country")
+
+mapCountryData(mapToPlot = t, nameColumnToPlot = "doc_count", numCats = 10, catMethod = "quantiles")
