e7045fb4cb71d92056f981533915e1469a4affa6
Jeanette Clark <jclark@nceas.ucsb.edu>
2020-03-16 14:01:07 -0700

    add another page to query

--- api-scopus-search.sh
+++ api-scopus-search.sh
@@ -2,7 +2,7 @@
 APIKEY=985f23cdfde84bdcb15229504fb644b4
 
 # multi-page ADC
- for pg in 0 26; do curl https://api.elsevier.com/content/search/scopus?query=ALL:10.18739\&date=2000-2019\&APIKey=${APIKEY}\&start=${pg} -o results/scopus-10.18739-2009-2019-pg${pg}.json; done
+ for pg in 0 26 52; do curl https://api.elsevier.com/content/search/scopus?query=ALL:10.18739\&date=2000-2019\&APIKey=${APIKEY}\&start=${pg} -o results/scopus-10.18739-2009-2019-pg${pg}.json; done
  for pg in 0 26 52; do curl https://api.elsevier.com/content/search/scopus?query=ALL:10.5065%2FD6*\&date=2009-2019\&APIKey=${APIKEY}\&start=${pg} -o results/scopus-10.5065-2009-2019-pg${pg}.json; done
 
 # multi-page KNB
