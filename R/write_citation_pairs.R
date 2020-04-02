9665ff144499a42f2db21bbd47c39bc915b0db58
MayaSamet <mayasamet@gmail.com>
2020-04-02 10:46:26 -0700

    reorganize + make json write work

--- R/write_citation_pairs.R
+++ R/write_citation_pairs.R
@@ -4,11 +4,11 @@ library(bib2df)
 
 
 # manually generated citations list
-cit <- read.csv("~/dataone-citations/citationlist.csv", stringsAsFactors = F)
+cit <- read.csv("DBO_citations/citationlist.csv", stringsAsFactors = F)
 
 # write list of citations to bib format
 bib <- cr_cn(dois = cit$publicationDOI, format = "bibtex")
-writeLines(unlist(bib), "~/dataone-citations/DBO_citations/all_citations.bib" )
+writeLines(unlist(bib), "DBO_citations/all_citations.bib" )
 
 # import as a dataframe
 df <- bib2df("~/dataone-citations/DBO_citations/all_citations.bib")
@@ -29,7 +29,6 @@ cit_full <- df %>%
   select(target_id, source_id, source_url, origin, title, publisher, journal, volume, page, year_of_publishing) %>% 
   mutate(id = NA, report = NA, metadata = NA, link_publication_date = NA) %>% 
   mutate(publisher = ifelse(publisher == "Elsevier {BV", "Elsevier", "Copernicus"))
-
 
 write_json(cit_full, "~/dataone-citations/DBO_citations/citations_export.json")
 
