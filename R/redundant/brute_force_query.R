91af590d83e376dd0e1f71dae533195d953f4b29
MayaSamet <mayasamet@gmail.com>
2020-04-01 17:22:06 -0700

    reorganize

--- R/redundant/brute_force_query.R
+++ R/redundant/brute_force_query.R
@@ -1,3 +1,10 @@
+# All code in this file contained in brute_force_query_ADC
+
+## IDEAS
+# use fulltext library docs.opensci.org/fulltext
+# rdatacite package
+# opencitations.net
+
 library(jsonlite)
 library(dplyr)
 library(xml2)
@@ -12,7 +19,7 @@ library(curl)
 # # multi-page KNB
 # for pg in 0 26; do curl https://api.elsevier.com/content/search/scopus?query==ALL:10.5063\&date=2009-2019\&APIKey=${APIKEY}\&start=${pg} -o results/scopus-10.5063-2009-2019-pg${pg}.json; done
 
-# query for ADC DOIs
+# query for all(?) ADC DOIs
 cn <- CNode("PROD")
 mn <- getMNode(cn, "urn:node:ARCTIC")
 
@@ -26,6 +33,7 @@ dois <- grep("doi", result$identifier, value = T) %>%
   gsub("doi:", "", .)
 
 # brute force query SCOPUS for each DOI
+
 t <- list()
 for (i in 1:length(dois)){
   t[[i]] <- fromJSON(curl(paste0("https://api.elsevier.com/content/search/scopus?query=ALL:",dois[i],"&APIKey=ae55f95a9d2f56c21147d3f9f6c4eef0")))
@@ -34,27 +42,16 @@ for (i in 1:length(dois)){
 # find the number of results per DOI
 res <- lapply(t, function(x){x$`search-results`$`opensearch:totalResults`})
 
-# filter out DOIs with no results
 t_working <- t[which(res != 0)]
 
-# pull out information for datasets with results
 t_results <- lapply(t_working, function(x){
   x$`search-results`$entry$search <- x$`search-results`$`opensearch:Query`$`@searchTerms`
   return(x$`search-results`$entry)
   })
 
-
 results <- do.call(bind_rows, t_results)
 
-# trim out unnecessary information
-results_slim <- results %>% 
-  dplyr::select(`prism:doi`, search) %>% 
-  mutate(search = gsub("ALL:", "", search)) %>% 
-  rename(journal = `prism:doi`,
-         adc_dataset = search)
 
-# left col - citer, right col - citee
-write.csv(results_slim, "~/dataone-citations/results/brute_force_results.csv", row.names = F)
 
 
 
