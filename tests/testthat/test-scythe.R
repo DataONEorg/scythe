1f772bd46c338e3b586d6d0dc4a6a5974c67a3f6
Matt Jones <gitcode@magisa.org>
2020-04-09 18:05:20 -0800

    Example citations that should be found while testing.
    We should add a more representative set to this list.

--- tests/testthat/test-scythe.R
+++ tests/testthat/test-scythe.R
@@ -1,7 +1,15 @@
 test_that("scythe", {
 
-    # TODO: Overall tests of the scythe package go here
+    # Overall tests of the scythe package go here
 
     # For now, ensure the test framework works
     expect_equal(2 * 2, 4)
+
+    # Load a set of example citations that should be found by the API
+    citations_file <- system.file("testdata","test-citations.csv",package="scythe")
+    citations <- read.csv(citations_file, stringsAsFactors = FALSE)
+    expect_s3_class(citations, "data.frame")
+
+    # TODO: Use scythe to search for each of these citations, and verify they are found
+
 })
