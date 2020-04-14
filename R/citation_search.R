072fb0d00d50f84d08fe177b222b18136a2a4643
Jeanette Clark <jclark@nceas.ucsb.edu>
2020-04-14 16:06:57 -0700

    enable handling of no results, remove tidyverse dependency, add example

--- R/citation_search.R
+++ R/citation_search.R
@@ -5,6 +5,8 @@
 #' @return tibble of matching dataset and publication identifiers
 #' @export
 #' @examples
+#' identifiers <- c("10.18739/A22274", "10.18739/A2D08X", "10.5063/F1T151VR")
+#' result <- citation_search_plos(identifiers)
 citation_search <- function(identifiers) {
     
     results <- rbind(citation_search_plos(identifiers) 
@@ -21,8 +23,6 @@ citation_search <- function(identifiers) {
 #' @return tibble of matching dataset and publication identifiers
 #' @export
 #' @importFrom rplos searchplos
-#' @importFrom dplyr mutate
-#' @importFrom dplyr rename
 #' @examples
 #' 
 #' identifiers <- c("10.18739/A22274", "10.18739/A2D08X", "10.5063/F1T151VR")
@@ -34,20 +34,25 @@ citation_search_plos <- function(identifiers) {
     results <- lapply(identifiers, function(x){
      rplos::searchplos(q = x,
                        fl = "id",
-                       fq = c("body", "supporting_information", "reference"),
                        limit = 1000)
         }
     )
-     
+    
     plos_results <- list()
     # assign dataset identifier to each result
-    for (i in 1:length(identifier)){
-        plos_results[[i]] <- results[[i]]$data %>% 
-            dplyr::mutate(dataset_id = identifier[i])
+    for (i in 1:length(results)){
+        if (results[[i]]$meta$numFound == 0){
+            plos_results[[i]] <- data.frame(id = NA, dataset_id = identifiers[i])
+        }
+        else if (results[[i]]$meta$numFound > 0){
+            plos_results[[i]] <- results[[i]]$data
+            plos_results[[i]]$dataset_id <- identifiers[i]
+        }
+
     }
     # bind resulting tibbles
-    plos_results <- do.call(rbind, plos_results) %>% 
-        rename(article_id = id)
+    plos_results <- do.call(rbind, plos_results)
+    names(plos_results)[which(names(plos_results) == "id")] <- "article_id"
     
     return(plos_results)
 }
