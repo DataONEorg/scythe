21599624cb771fe89674503eceb98a0ba11c5fe4
Jeanette Clark <jclark@nceas.ucsb.edu>
2021-03-09 10:43:51 -0700

    remove space in test which makes for extra mangled html

--- tests/testthat/test-citation_search.R
+++ tests/testthat/test-citation_search.R
@@ -1,6 +1,6 @@
 test_that("citation_search identifer input looks like an identifier", {
     
-    identifiers <- c("some text", "10.18739/A25D8NF4H")
+    identifiers <- c("sometext", "10.18739/A25D8NF4H")
 
     expect_warning(citation_search(identifiers))
 })
