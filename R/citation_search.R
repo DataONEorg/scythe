87376c850acb0b240b190f63c9f661e41fee4e5e
Matt Jones <gitcode@magisa.org>
2021-03-12 15:39:29 -0900

    Revert "Refactor citation_search to not hardcode sources."
    This reverts commit c83fe5e04364ca1eb91a73e129156539bc558d26.

--- R/citation_search.R
+++ R/citation_search.R
@@ -12,21 +12,26 @@
 #' }
 citation_search <- function(identifiers,
                             sources = c("plos", "scopus", "springer")) {
-  if (any(!grepl("10\\.|urn:uuid", identifiers))) {
-    warning(call. = FALSE,
-            "One or more identifiers does not appear to be a DOI or uuid")
-  }
 
-  # run the 'citation_search_*' function for each source
-  for (source in sources) {
-    search_function <- paste0(source, " <- citation_search_", source, "(identifiers)")
-    eval(parse(text = search_function))
-  }
-
-  # combine all of the resulting data frames and return the result df
-  bind_function <- paste0("rbind(", sources, ")")
-  result <- eval(parse(text = bind_function))
+    if (any(!grepl("10\\.|urn:uuid", identifiers))){
+        warning(call. = FALSE, "One or more identifiers does not appear to be a DOI or uuid")
+    }
+    
+    if ("plos" %in% sources){
+      plos <- citation_search_plos(identifiers)
+    } else plos <- NULL
+  
+    if ("scopus" %in% sources){
+      scopus <- citation_search_scopus(identifiers)
+    } else scopus <- NULL
+  
+    if ("springer" %in% sources){
+      springer <- citation_search_springer(identifiers)
+    } else springer <- NULL
+  
+  result <- rbind(plos, scopus, springer)
   return(result)
+
 }
 
 # Check identifiers to remove characters that interfere with query strings
@@ -42,6 +47,7 @@ check_identifiers <- function(identifiers){
     identifiers <- gsub("(doi:)|(urn:uuid:)", "", identifiers)
     message("Identifier prefix (doi: or urn:uuid) has been stripped out of the search term.")
   }
+
 
   return(identifiers)
 }
