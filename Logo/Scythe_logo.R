38a59fe8c287ed1d4f2fe5a30858606e13519b43
Althea Marks <theamarks@gmail.com>
2022-03-16 13:04:38 -0700

    Added required packages to create hexsticker

--- Logo/Scythe_logo.R
+++ Logo/Scythe_logo.R
@@ -1,8 +1,17 @@
 ### Great Logo for Scythe Package
 ### Althea Marks
+### Created 2022_3_16
 
 ## Install packages
-#install.packages("hexSticker")
-library(hexSticker)
 
-##
+#install.packages("sysfonts")
+#install.packages("magick")
+#install.packages("hexSticker")
+#install.packages("tidyverse")
+
+library(magick)
+library(hexSticker)
+library(sysfonts)
+library(tidyverse)
+
+## 
