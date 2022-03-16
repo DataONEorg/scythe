d45dd0dba363ecaf799862e58f68360441a21974
Althea Marks <theamarks@gmail.com>
2022-03-16 15:10:05 -0700

    Tweaked hexsticker. Dark grey color theme, package title position, resolution, image position.

--- Logo/Scythe_logo.R
+++ Logo/Scythe_logo.R
@@ -20,14 +20,22 @@ library(tidyverse)
 
 wikicomm_image <- "Logo/Glazed_pottery_tile_man_with_a_scythe_01.jpg"
 
-scythe_logo <- sticker(wikicomm_image,
-                       s_x = 1,
+scythe_logo <- sticker(wikicomm_image, #subplot image
+                       s_x = 1, #subplot position relative to 1(center)
                        s_y = 1,
-                       package = "Scythe",
-                       p_size = 20,
-                       p_x = 1,
-                       p_y = 1,
-                       p_color = "black",
+                       s_width = 1.1,
+                       s_height = 1.1,
+                       package = "Scythe", #text displayed for package name
+                       p_size = 20, #font size
+                       p_x = 1, # position font
+                       p_y = 0.45,
+                       p_color = "grey30",
+                       dpi = 1000, # plot resolution
+                       asp = 1, # aspect ratio of image file
+                       h_color = "grey30", # boarder color
+                       h_size = 3, # boarder size
+                       white_around_sticker = T,
+                       spotlight = F,
                        filename = "Logo/Scythe_Hex_Sticker.png")
 
 scythe_logo
