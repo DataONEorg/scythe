a9ca649ec292ba05fb5924af072a72b68ac80422
Althea Marks <theamarks@gmail.com>
2022-03-16 14:45:19 -0700

    Added basic hexSticker arguments. Succsessful creation of hex sticker

--- Logo/Scythe_logo.R
+++ Logo/Scythe_logo.R
@@ -14,4 +14,20 @@ library(hexSticker)
 library(sysfonts)
 library(tidyverse)
 
-## 
+## Construct hex sticker logo
+
+# import image 
+
+wikicomm_image <- "Logo/Glazed_pottery_tile_man_with_a_scythe_01.jpg"
+
+scythe_logo <- sticker(wikicomm_image,
+                       s_x = 1,
+                       s_y = 1,
+                       package = "Scythe",
+                       p_size = 20,
+                       p_x = 1,
+                       p_y = 1,
+                       p_color = "black",
+                       filename = "Logo/Scythe_Hex_Sticker.png")
+
+scythe_logo
