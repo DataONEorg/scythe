b9c82dcf0beec062313126a86dac21f4847d2dc6
Althea Marks <theamarks@gmail.com>
2022-04-04 18:40:37 -0700

    Updated if() for 0 returned results. Full function running clean. Need to use length() on list not nrow(). Added annotations.

--- R/citation_search_xDD.R
+++ R/citation_search_xDD.R
@@ -11,19 +11,12 @@
 #' \dontrun{
 #' identifiers <- c("10.18739/A22274", "10.18739/A2D08X", "10.5063/F1T151VR")
 #' result <- citation_search_xDD(identifiers)
-#' results_list <- jsonlite::fromJSON(curl::curl(paste0("https://xdd.wisc.edu/api/snippets?term=", "10.18739/A2D08X")))
 #' }
-#' 
-#test_citations <- read_csv("inst/testdata/test-citations.csv")
-#test_citations <- test_citations[,6]
+#'
 
 citation_search_xDD <- function(identifiers) {
   
   identifiers <- check_identifiers(identifiers)
-  
- # if (length(identifiers) > 8){
- #   message(paste0("Your result will take ~", length(identifiers)/9 ," seconds to return, since this function is rate limited to 9 calls per second."))
- # }
   
   identifiers_enc <- lapply(identifiers, utils::URLencode, reserved = TRUE) # URLencode - percent-encode in URL
   identifiers_enc <- unlist(identifiers_enc) # given a list, produces a vector
@@ -39,23 +32,23 @@ citation_search_xDD <- function(identifiers) {
                             article_id = character(),
                             article_title = character())
   
-  # extract relevant information from raw results 
+  # extract select information from results 
   for (i in 1:length(results)) {
-    if (nrow(results[[i]]$success$data) == 0){
+    if (length(results[[i]]$success$data) == 0){ # if no returned results, do this
       df <- data.frame(dataset_id = identifiers[i],
                                 article_id = NA,
                                 article_title = NA)
     }
-    else if (nrow(results[[i]]$success$data) > 0){    
+    else if (nrow(results[[i]]$success$data) > 0){ # if results returned, do this
     dataset_id <- identifiers[i]
     article_id <- results[[i]]$success$data$doi
     article_title <- results[[i]]$success$data$title
     df <- data.frame(dataset_id, article_id, article_title)
     }
     
-    else (print("input does not meet either conditions within"))
+    else (print("input does not meet either conditions within")) # need closing else statement?
     
-  xDD_results <- rbind(xDD_results, df)
+  xDD_results <- rbind(xDD_results, df) # bind subsequent results to previous results data.frame
   }
   
   return(xDD_results)
