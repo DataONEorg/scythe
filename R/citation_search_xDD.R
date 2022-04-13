6b849fb51646e462833547a0e6299a879d0fe02d
Althea Marks <theamarks@gmail.com>
2022-04-13 12:43:11 -0700

    Added dataset doi to test identifiers that produced 2 results in xDD

--- R/citation_search_xDD.R
+++ R/citation_search_xDD.R
@@ -9,7 +9,7 @@
 #' @return tibble of publications and their identifiers that contain 
 #' @examples
 #' \dontrun{
-#' identifiers <- c("10.18739/A22274", "10.18739/A2D08X", "10.5063/F1T151VR")
+#' identifiers <- c("10.18739/A22274", "10.18739/A2D08X", "10.5063/F1T151VR", "10.18739/A29K97")
 #' result <- citation_search_xDD(identifiers)
 #' }
 #'
