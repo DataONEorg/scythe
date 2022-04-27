a69ef6059c66d5da1434a6fe0ae57532331038a7
Althea Marks <theamarks@gmail.com>
2022-04-27 11:00:50 -0700

    added underscore to output source

--- R/citation_search_plos.R
+++ R/citation_search_plos.R
@@ -57,7 +57,7 @@ citation_search_plos <- function(identifiers) {
     plos_results <- do.call(rbind, plos_results)
     names(plos_results)[which(names(plos_results) == "id")] <- "article_id"
     names(plos_results)[which(names(plos_results) == "title")] <- "article_title"
-    names(plos_results)[which(names(plos_results) == "source")] <- "article source"
+    names(plos_results)[which(names(plos_results) == "source")] <- "article_source"
     plos_results <- plos_results[complete.cases(plos_results), ] # remove incomplete cases (NAs)
 
     return(plos_results)
