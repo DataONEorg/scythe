ee8926bf5d4515d3b1683626aa775e18a2906cea
Althea Marks <theamarks@gmail.com>
2022-05-02 16:26:31 -0700

    Fixed error (column names not match) in plos search function rbind.

--- R/citation_search_plos.R
+++ R/citation_search_plos.R
@@ -40,9 +40,9 @@ citation_search_plos <- function(identifiers) {
     # assign dataset identifier to each result
     for (i in 1:length(results)){
         if (results[[i]]$meta$numFound == 0 | is.null(results[[i]])){
-            plos_results[[i]] <- data.frame(article_id = NA,
+            plos_results[[i]] <- data.frame(id = NA,
                                             dataset_id = identifiers[i],
-                                            article_title = NA,
+                                            title = NA,
                                             source = "plos")
         }
         else if (results[[i]]$meta$numFound > 0){
@@ -57,7 +57,6 @@ citation_search_plos <- function(identifiers) {
     plos_results <- do.call(rbind, plos_results)
     names(plos_results)[which(names(plos_results) == "id")] <- "article_id"
     names(plos_results)[which(names(plos_results) == "title")] <- "article_title"
-    #names(plos_results)[which(names(plos_results) == "source")] <- "article_source"
     plos_results <- plos_results[complete.cases(plos_results), ] # remove incomplete cases (NAs)
 
     return(plos_results)
