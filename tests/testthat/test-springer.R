2c6c514192f94465894bbf691ec54aa218a96046
Althea Marks <theamarks@gmail.com>
2022-05-10 09:20:41 -0700

    Created springer specific test script to test if single known doi citation is found

---
+++ tests/testthat/test-springer.R
@@ -0,0 +1,13 @@
+test_that("springer finds single known doi", {
+  
+  skip_on_cran()
+  
+  # Pull single test doi citation from test-citations.csv
+  test_cit <- get_test_doi("springer")
+  
+  # Search for single test doi using citation_search_springer() function
+  springer_res <- citation_test_doi("springer") # Need to key API keys in order for this to work
+  
+  expect_setequal(test_cit$pub_id, springer_res$article_id)
+})
+
