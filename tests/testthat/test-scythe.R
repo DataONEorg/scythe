e8abf2bb700afe69481aae524b995ead050ca83d
Althea Marks <theamarks@gmail.com>
2022-05-10 15:54:53 -0700

    Trying to add NULL stop to test-scythe to stop test if API not available. Not working

--- tests/testthat/test-scythe.R
+++ tests/testthat/test-scythe.R
@@ -23,9 +23,14 @@ test_that("scythe finds multiple dois", {
     # remove non-doi identifiers
     doi <- citations %>% 
       filter(grepl("^10\\.", dataone_pid)) 
+    
+    # stop test if NULL (needed API key not found)
+    if(is.null(keyed_sources)){message(paste0("API key is NULL for ", library))}
+    # search for single known doi citation in specified library/source
+    else{
              
     # Use scythe to search for each of the doi citations, and verify they are found
     results_doi <- citation_search(doi$dataone_pid, sources = keyed_sources)
     expect_true(all(doi$pub_id %in% results_doi$article_id))
     
-})
+}})
