ff179ed01b2b90b3dd7814dc6bfb9ecddd3151eb
Jeanette Clark <jclark@nceas.ucsb.edu>
2024-09-09 22:40:22 +0000

    styling and remove unnecessary lapply

--- R/citation_search_scopus.R
+++ R/citation_search_scopus.R
@@ -14,17 +14,17 @@
 #' }
 citation_search_scopus <- function(identifiers) {
   identifiers <- check_identifiers(identifiers)
-  
+
   if (length(identifiers) > 8) {
     message(
       paste0(
         "Your result will take ~",
-        length(identifiers) / 9 ,
+        length(identifiers) / 9,
         " seconds to return, since this function is rate limited to 9 calls per second."
       )
     )
   }
-  
+
   key <- scythe_get_key("scopus")
   if (is.na(key)) {
     warning(
@@ -32,8 +32,7 @@ citation_search_scopus <- function(identifiers) {
     )
     return()
   }
-  identifiers_enc <-
-    lapply(identifiers, utils::URLencode, reserved = TRUE)
+  identifiers_enc <- utils::URLencode(identifiers, reserved = TRUE)
   identifiers_enc <- unlist(identifiers_enc)
   results <- list()
   for (i in 1:length(identifiers_enc)) {
@@ -47,7 +46,7 @@ citation_search_scopus <- function(identifiers) {
         )
       ))
   }
-  
+
   # initialize df for storing results in orderly fashion
   scopus_results <- data.frame(
     article_id = character(),
@@ -55,31 +54,27 @@ citation_search_scopus <- function(identifiers) {
     dataset_id = character(),
     source = character()
   )
-  
+
   # extract relevant information from raw results
   for (i in 1:length(results)) {
-    
-    
     article_id <-
       results[[i]][["search-results"]][["entry"]][["prism:doi"]]
     article_title <-
       results[[i]][["search-results"]][["entry"]][["dc:title"]]
-    
+
     num_citations <- length(article_id)
-    
+
     dataset_id <- rep(identifiers[i], num_citations)
     source <- rep("scopus", num_citations)
-    scopus_results <-
-      rbind(scopus_results,
-            data.frame(article_id,
-                       article_title,
-                       dataset_id,
-                       source))
+    scopus_results <- rbind(
+      scopus_results,
+      data.frame(article_id, article_title, dataset_id, source)
+    )
   }
-  
+
   # clean up dois
   scopus_results$dataset_id <-
     gsub("ALL:", "", scopus_results$dataset_id)
-  
+
   return(scopus_results)
 }
